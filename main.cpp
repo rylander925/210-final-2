b72472ba611abf5fabe04791e2be5cb0ba360238
rylander925 <rylander925@gmail.com>
2025-12-08 16:45:53 -0800

    Adding specifications

--- main.cpp
+++ main.cpp
@@ -2,3 +2,36 @@
 COMSC-210 | Final 2 | Rylan Der
 IDE Used: Visual Studio Code
 */
+
+/*
+Milestone 1: Coffee booth queue struct
+    * Use linked list as core data structure
+    * Struct contains name & drink order
+    * Use LLM to create arrays of names/drinks
+*/
+
+/*
+Milestone 2: Run sim for 10 rounds
+    * Init queue w/ 3 customers
+    * Possibilities:
+    *   0.5 join queue
+    *   1.0 serve head (if not empty)
+*/
+
+/*
+Milestone 3: Repeat 1 & 2 with a muffin booth
+    * Use a deque as core data structure
+    * run 10 round sim
+        * same possibilities
+    * (Can reuse names, make new muffin flavor array)
+*/
+
+/*
+Milestone 4: Repeat 1 & 2 w/ friendship bracelets
+    * Use vector
+*/
+
+/*
+Milestone 5: Repeat 1 & 2 w/ struct of your choice
+    * use queue
+*/
